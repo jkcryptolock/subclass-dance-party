d2731f6fb89283c0f7d2b2774ec673c0f080ecae
jkcryptolock <jkconno@gmail.com>
2019-07-23 16:09:31 -0500

    resolving merge issues

--- src/buuDancer.js
+++ src/buuDancer.js
@@ -1,10 +1,6 @@
 var BuuDancer = function(top, left, timeBetweenSteps) {
     Dancer.call(this, top, left, timeBetweenSteps);
-<<<<<<< HEAD
     this.$node = $('<img src="./images/buu.gif" class="buu dbz">')
-=======
-    this.$node = $('<img src="./images/buu.gif" class="buu">')
->>>>>>> 935a06846e2de616e8055c1ac0e536a7fbce4e0c
     this.setPosition(top, left);
   };
   
